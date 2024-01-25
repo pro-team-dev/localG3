615153e1d6d40b98296cb011076ff10255faa385
Atul0002 <1atultiwari062@gmail.com>
2024-01-26 02:10:26 +0545

    Fix map display issue in ongoing.tsx

--- app/(home)/ongoing.tsx
+++ app/(home)/ongoing.tsx
@@ -53,23 +53,12 @@ const OnGoing = () => {
   }, [location]);
 
   return (
-<<<<<<< HEAD
     <View style={{ height: Dimensions.get("window").height / 3 }}>
       <View>
-        {(locationArr && locationArr.lat) && <MapsComponent
+        {(locationArr) && <MapsComponent
           locations={[locationArr]}
           cameraLocation={[locationArr.lng, locationArr.lat]}
         />}
-=======
-    <View style={{ height: Dimensions.get("window").height - 130 }}>
-      <View style={{ flex: 1 }}>
-        {locationArr && (
-          <MapsComponent
-            locations={[locationArr]}
-            cameraLocation={[locationArr.lng, locationArr.lat]}
-          />
-        )}
->>>>>>> bd066c0ecb64af8721de7ee757537f3ea1637297
       </View>
       {data && <Card data={data} />}
     </View>
@@ -79,7 +68,6 @@ const OnGoing = () => {
 const Card = ({ data }) => {
   return (
     <View style={styles.card}>
-<<<<<<< HEAD
       <ScrollView>
         <Text style={styles.text}>Tour ID: {data.tour_id}</Text>
         <Text style={styles.text}>Location: {data.locations[0].name}</Text>
@@ -100,39 +88,6 @@ const Card = ({ data }) => {
         <Text style={styles.text}>Guide: {data.guide}</Text>
         <Text style={styles.text}>Offer: {data.offer[0]}</Text>
       </ScrollView>
-=======
-      <Text style={styles.text}>Location: {data.locations[0].name}</Text>
-      <Text style={styles.text}>Status: {data.status}</Text>
-      <Text style={styles.text}>Price: {data.price}</Text>
-      <Text style={styles.text}>Duration: {data.duration}</Text>
-      <Text style={styles.text}>No. of People: {data.no_of_people}</Text>
-      <Text style={styles.text}>
-        Travel Coverage: {data.travel_coverage ? "Yes" : "No"}
-      </Text>
-      <Text style={styles.text}>
-        Food Coverage: {data.food_coverage ? "Yes" : "No"}
-      </Text>
-      <Text style={styles.text}>Personal Request: {data.personal_request}</Text>
-      {/* <Text style={styles.text}>Created At: {data.created_at}</Text> */}
-      {/* <Text style={styles.text}>Updated At: {data.updated_at}</Text> */}
-      {/* <Text style={styles.text}>Tourist: {data.tourist}</Text> */}
-      <Text style={styles.text}>Guide: {data.guide}</Text>
-      {/* <Text style={styles.text}>Offer: {data.offer[0]}</Text> */}
-      <View
-        style={{ flexDirection: "row", justifyContent: "flex-end", gap: 5 }}
-      >
-        <CustomButton
-          style={{ width: 100, backgroundColor: "rgb(200,100,100)" }}
-          title="Cancel"
-          // onPress={() => handleCancel()}
-        />
-        <CustomButton
-          style={{ width: 150 }}
-          title="Complete"
-          // onPress={() => handleComplte()}
-        />
-      </View>
->>>>>>> bd066c0ecb64af8721de7ee757537f3ea1637297
     </View>
   );
 };
